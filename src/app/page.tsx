b05f51fee22cb7fde8760ebbef9143e21f83348d
antoinepiney <antoine.piney@gmail.com>
2025-01-30 18:28:51 +0100

    fix: layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ import ImageBreak from "@/sections/ImageBreak";
 
 export default function Home() {
   return (
-    <>
-      <main className={styles.main}>
-        <Hero />
-        <ImageBreak />
-      </main>
-    </>
+    <main className={styles.main}>
+      <Hero />
+      <ImageBreak />
+    </main>
   );
 }
