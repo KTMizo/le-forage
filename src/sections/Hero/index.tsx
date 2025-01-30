701ff19bdd932384b03c4bb617ce880040329a79
antoinepiney <antoine.piney@gmail.com>
2025-01-30 16:59:23 +0100

    news: Hero + Image

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./Hero.module.css";
 import Button from "@/components/UI/Button";
 
@@ -8,19 +9,24 @@ const Hero = () => {
       <div className={styles.container}>
         {/* Logo */}
         <div className={styles.logo}>
-          <img src="/assets/svg/logo/logo.svg" alt="Le Forage Logo" />
+          <Image
+            src="/assets/svg/logo/logo.svg"
+            alt="Le Forage Logo"
+            width={350}
+            height={160}
+            priority
+          />
         </div>
 
         {/* Main Content */}
         <div className={styles.content}>
           <div className={styles.contentText}>
-            <h1 className={styles.title}>L'art de sonder le sol</h1>
+            <h1 className={styles.title}>{`L'art de sonder le sol`}</h1>
 
             <p className={styles.description}>
-              Nous accompagnons de nombreux bureaux d'études géotechniques et
-              environnementaux dans la réussite de leurs projets.
+              {`Nous accompagnons de nombreux bureaux d'études géotechniques et environnementaux dans la réussite de leurs projets.`}
             </p>
-          </div>{" "}
+          </div>
           <Button variant="outline" href="/destination" showArrow>
             Demandez un devis
           </Button>
--- src/sections/ImageBreak/index.tsx
+++ src/sections/ImageBreak/index.tsx
@@ -1,14 +1,19 @@
+// ImageBreak/index.tsx
 import React from "react";
+import Image from "next/image";
 import styles from "./ImageBreak.module.css";
 
 const ImageBreak = () => {
   return (
     <div className={styles.imageWrapper}>
-      {/* ImageWrapper */}
-      <img
+      <Image
         className={styles.image}
         src="/assets/images/hero-cover.jpg"
         alt="Le Forage"
+        width={1920} // Ajustez selon vos besoins
+        height={1080} // Ajustez selon vos besoins
+        priority // Pour LCP (Largest Contentful Paint)
+        quality={85} // Équilibre entre qualité et performance
       />
     </div>
   );
