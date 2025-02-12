fb610a6bc91bfb98ae47cac660dc85314f5db217
antoinepiney <antoine.piney@gmail.com>
2025-02-12 21:34:43 +0100

    WordpressProduction 1st push

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "localhost",
-      "admin.leforage.fr", // Ajout du domaine de production WordPress
-    ],
+    domains: ["localhost", "admin.leforage.fr"],
   },
   webpack(config) {
-    // Configuration SVG
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
   },
-  // Ajout des optimisations pour résoudre les erreurs de préchargement
-  experimental: {
+  // Retirez experimental.optimizeCss car il cause des problèmes
+  /* experimental: {
     optimizeCss: true,
     scrollRestoration: true,
-  },
-  // Amélioration de la gestion des styles
-  optimizeFonts: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
+  }, */
 };
 
 export default nextConfig;
