7869c6b64c96f3b52d0d1af1fd3f2bf1ed7fa230
antoinepiney <antoine.piney@gmail.com>
2025-02-12 22:10:02 +0100

    WordpressProduction 1st push

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -25,7 +25,7 @@ const Hero: React.FC<HeroProps> = ({ data }) => {
   const buttonData = {
     variant: buttonVariant,
     text: data?.button?.text ?? "Demandez un devis",
-    url: data?.button?.url ?? "/destination",
+    url: data?.button?.url ?? "/",
     showArrow: data?.button?.showArrow ?? true,
   };
 
